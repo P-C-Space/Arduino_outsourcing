83a10e2df79df5beba6683eabc71fa03b144158d
jeongwoo <sun62073812@gmail.com>
2023-05-13 19:14:15 +0900

    project

---
+++ 코드/bluetooth_master_test.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+int Tx = 7; // Tx Pin number
+int Rx = 8; // Rx Pin number
+SoftwareSerial BTSerial(Tx,Rx); // bluetooth 모듈 핀 설정
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // 시리얼 모니터 9600 보드레이트 통신선언
+  BTSerial.begin(9600); // bluetooth 모듈 9600 보드레이트 통신선언
+  Serial.println("Initialized"); // 모니터 출력
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BTSerial.print("CONNECT");
+  delay(1000);
+}
